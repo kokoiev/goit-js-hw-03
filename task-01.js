a337d6cfb679b4107fef052f80f94aeec7cae20c
sqralan <sqralan@ukr.net>
2020-10-06 04:49:40 +0300

    finish

--- task-01.js
+++ task-01.js
@@ -15,8 +15,5 @@ const entries = Object.entries(user);
 console.log(entries);
 
 for (const entry of entries) {
-  const key = entry[0];
-  const value = entry[1];
-
-  console.log(`${key}: ${value}`);
+  console.log(`${entry[0]}: ${entry[1]}`);
 }
